da97fa8aae9bcd607dbf66be99e992f3e98f8799
ssisaias <luiz.isaias95@gmail.com>
2020-07-19 19:22:27 -0300

    Create transaction and category

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,49 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: TransactionDTO): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    // Check if Category exists
+    const categoryRepository = getRepository(Category);
+    const categoryFound = await categoryRepository.findOne({
+      where: { title: category },
+    });
+    let newCategory: Category = new Category();
+    if (!categoryFound) {
+      newCategory = categoryRepository.create({
+        title: category,
+      });
+      newCategory = await categoryRepository.save(newCategory);
+    }
+
+    const newTransaction = transactionRepository.create({
+      title,
+      type,
+      value,
+      category: !categoryFound ? newCategory : categoryFound,
+    });
+
+    transactionRepository.save(newTransaction);
+
+    return newTransaction;
   }
 }
 
